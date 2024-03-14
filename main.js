df33c217a1686aec34a6715ca5e4e403f1154d5f
karolinapole <155312413+karolinapole@users.noreply.github.com>
2024-03-14 12:14:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let table = document.querySelector(".table");
         let workbook = XLSX.read(await (await fetch("PokeTCG.xlsx")).arrayBuffer());
-        console.log(workbook);
         let worksheet = workbook.SheetNames;
         worksheet.forEach(name => {
             let html = XLSX.utils.sheet_to_html(workbook.Sheets[name]);
