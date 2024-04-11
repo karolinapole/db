d71c69d00732f69b5b1092dd56949674c598cbab
karolinapole <155312413+karolinapole@users.noreply.github.com>
2024-04-11 12:04:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ let table = document.querySelector(".table");
                     `;
         })
     
+let table = document.querySelector(".table");
+        let workbook = XLSX.read(await (await fetch("PokeTCG.xlsx")).arrayBuffer());
+        console.log(workbook);
+        let worksheet = workbook.SheetNames;
+        worksheet.forEach(name => {
+            let html = XLSX.utils.sheet_to_html(workbook.Sheets[name]);
+            table.innerHTML += `
+                    <h3>${name}</h3>${html}
+                    `;
+        })
+    
