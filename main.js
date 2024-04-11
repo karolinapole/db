0298479938976e66e940eddfbba94ab78a5ca996
karolinapole <155312413+karolinapole@users.noreply.github.com>
2024-04-11 12:04:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 let table = document.querySelector(".table");
         let workbook = XLSX.read(await (await fetch("PokeTCG.xlsx")).arrayBuffer());
-        let worksheet = workbook.SheetNames;
-        worksheet.forEach(name => {
-            let html = XLSX.utils.sheet_to_html(workbook.Sheets[name]);
-            table.innerHTML += `
-                    <h3>${name}</h3>${html}
-                    `;
-        })
-    
-let table = document.querySelector(".table");
-        let workbook = XLSX.read(await (await fetch("PokeTCG.xlsx")).arrayBuffer());
         console.log(workbook);
         let worksheet = workbook.SheetNames;
         worksheet.forEach(name => {
